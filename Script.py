6f49c9d14a2c0edc1ed3b39547b15b42ca0f5b9b
ᗪᗩᖇK ✞Oᖇᗪ <85796566+Judson-web@users.noreply.github.com>
2021-10-30 11:33:55 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -18,89 +18,89 @@ Hᴇʀᴇ Is Tʜᴇ Hᴇʟᴘ Fᴏʀ MY Cᴏᴍᴍᴀɴᴅs."""
 - <a href=https://t.me/STMbOTsUPPORTgROUP>Tᴇᴀᴍ Mᴇᴘʜɪsᴛᴏ</a>"""
     MANUELFILTER_TXT = """Hᴇʟᴘ: <b>Fɪʟᴛᴇʀs</b>
 
-- 𝓕ⲓ𝓵ⲧⲉꞅ ⲓ𝛓 ⲧⲏⲉ 𝓯ⲉⲁⲧ𐌵ꞅⲉ ⲱⲉꞅⲉ 𐌵𝛓ⲉꞅ𝛓 ⲥⲁⲛ 𝛓ⲉⲧ ⲁ𐌵ⲧⲟⲙⲁⲧⲉⲇ ꞅⲉⲣ𝓵ⲓⲉ𝛓 𝓯ⲟꞅ ⲁ ⲣⲁꞅⲧⲓⲥ𐌵𝓵ⲁꞅ ⲕⲉⲩⲱⲟꞅⲇ ⲁⲛⲇ ⲙⲉⲣⲏⲓ𝛓ⲧⲟ ⲱⲓ𝓵𝓵 ꞅⲉ𝛓ⲣⲟⲛⲇ ⲱⲏⲉⲛⲉ𝓿ⲉꞅ ⲁ ⲕⲉⲩⲱⲟꞅⲇ ⲓ𝛓 𝓯ⲟ𐌵ⲛⲇ ⲧⲏⲉ ⲙⲉ𝛓𝛓ⲁ𝓰ⲉ
-
-<b>ⲚⲞⲦⲈ:</b>
-1. ⲉ𝓿ⲁ ⲙⲁꞅⲓⲁ 𝛓ⲏⲟ𐌵𝓵ⲇ ⲏⲁ𝓿ⲉ ⲁⲇⲙⲓⲛ ⲣꞅⲓ𝓿ⲓ𝓵𝓵ⲁ𝓰ⲉ.
-2. ⲟⲛ𝓵ⲩ ⲁⲇⲙⲓⲛ𝛓 ⲥⲁⲛ ⲁⲇⲇ 𝓯ⲓ𝓵ⲧⲉꞅ𝛓 ⲓⲛ ⲁ ⲥⲏⲁⲧ.
-3. ⲁ𝓵ⲉꞅⲧ ⲃ𐌵ⲧⲧⲟⲛ𝛓 ⲏⲁ𝓿ⲉ ⲁ 𝓵ⲓⲙⲓⲧ ⲟ𝓯 64 ⲥⲏⲁꞅⲁⲥⲧⲉꞅ𝛓.
-
-<b>Ⲥⲟⲙⲙⲁⲛⲇ𝛓 ⲁⲛⲇ ⴑ𝛓ⲁ𝓰ⲉ:</b>
-• /filter - <code>ⲁⲇⲇ ⲁ 𝓯ⲓ𝓵ⲧⲉꞅ ⲓⲛ ⲥⲏⲁⲧ</code>
-• /filters - <code>𝓵ⲓ𝛓ⲧ ⲁ𝓵𝓵 ⲧⲏⲉ 𝓯ⲓ𝓵ⲧⲉꞅ𝛓 ⲟ𝓯 ⲁ ⲥⲏⲁⲧ</code>
-• /del - <code>ⲇⲉ𝓵ⲉⲧⲉ ⲁ 𝛓ⲣⲉⲥⲓ𝓯ⲓⲥ 𝓯ⲓ𝓵ⲧⲉꞅ ⲓⲛ ⲥⲏⲁⲧ</code>
-• /delall - <code>ⲇⲉ𝓵ⲉⲧⲉ ⲧⲏⲉ ⲱⲏⲟ𝓵ⲉ 𝓯ⲓ𝓵ⲧⲉꞅ𝛓 ⲓⲛ ⲁ ⲥⲏⲁⲧ (ⲥⲏⲁⲧ ⲟⲱⲛⲉꞅ ⲟⲛ𝓵ⲩ)</code>"""
-    BUTTON_TXT = """Help: <b>Buttons</b>
-
-- Mephisto Supports both url and alert inline buttons.
+- Fɪʟᴛᴇʀ ɪs ᴛʜᴇ ғᴇᴀᴛᴜʀᴇ ᴡᴇʀᴇ ᴜsᴇʀs ᴄᴀɴ sᴇᴛ ᴀᴜᴛᴏᴍᴀᴛᴇᴅ ʀᴇᴘʟɪᴇs ғᴏʀ ᴀ ᴘᴀʀᴛɪᴄᴜʟᴀʀ ᴋᴇʏᴡᴏʀᴅ ᴀɴᴅ ᴛᴇssᴀ ᴡɪʟʟ ʀᴇsᴘᴏɴᴅ ᴡʜᴇɴᴇᴠᴇʀ ᴀ ᴋᴇʏᴡᴏʀᴅ ɪs ғᴏᴜɴᴅ ᴛʜᴇ ᴍᴇssᴀɢᴇ
 
 <b>NOTE:</b>
-1. Telegram will not allows you to send buttons without any content, so content is mandatory.
-2. Mephisto supports buttons with any telegram media type.
-3. Buttons should be properly parsed as markdown format
+1. ᴇᴠᴀ ᴍᴀʀɪᴀ sʜᴏᴜʟᴅ ʜᴀᴠᴇ ᴀᴅᴍɪɴ ᴘʀɪᴠɪʟʟᴀɢᴇ.
+2. ᴏɴʟʏ ᴀᴅᴍɪɴs ᴄᴀɴ ᴀᴅᴅ ғɪʟᴛᴇʀs ɪɴ ᴀ ᴄʜᴀᴛ.
+3. ᴀʟᴇʀᴛ ʙᴜᴛᴛᴏɴs ʜᴀᴠᴇ ᴀ ʟɪᴍɪᴛ ᴏғ 64 ᴄʜᴀʀᴀᴄᴛᴇʀs.
 
-<b>URL buttons:</b>
-<code>[Button Text](buttonurl:https//t.me/Mephistov3_bot)</code>
+<b>Cᴏᴍᴍᴀɴᴅs ᴀɴᴅ Usᴀɢᴇ:</b>
+• /filter - <code>ᴀᴅᴅ ᴀ ғɪʟᴛᴇʀ ɪɴ ᴄʜᴀᴛ</code>
+• /filters - <code>ʟɪsᴛ ᴀʟʟ ᴛʜᴇ ғɪʟᴛᴇʀs ᴏғ ᴀ ᴄʜᴀᴛ</code>
+• /del - <code>ᴅᴇʟᴇᴛᴇ ᴀ sᴘᴇᴄɪғɪᴄ ғɪʟᴛᴇʀ ɪɴ ᴄʜᴀᴛ</code>
+• /delall - <code>ᴅᴇʟᴇᴛᴇ ᴛʜᴇ ᴡʜᴏʟᴇ ғɪʟᴛᴇʀs ɪɴ ᴀ ᴄʜᴀᴛ (ᴄʜᴀᴛ ᴏᴡɴᴇʀ ᴏɴʟʏ)</code>"""
+    BUTTON_TXT = """Hᴇʟᴘ: <b>Bᴜᴛᴛᴏɴs</b>
 
-<b>Alert buttons:</b>
-<code>[Button Text](buttonalert:This is an alert message)</code>"""
-    AUTOFILTER_TXT = """Help: <b>Auto Filter</b>
-
-<b>NOTE:</b>
-1. Make me the admin of your channel if it's private.
-2. make sure that your channel does not contains cam rip, porn and fake files.
-3. Forward the last message to me with quotes.
- I'll add all the files in that channel to my db."""
-    CONNECTION_TXT = """Help: <b>Connections</b>
-
-- Used to connect bot to PM for managing filters 
-- it helps to avoid spamming in groups.
+- Mᴇᴘʜɪsᴛᴏ Sᴜᴘᴘᴏʀᴛs ʙᴏᴛʜ ᴜʀʟ ᴀɴᴅ ᴀʟᴇʀᴛ ɪɴʟɪɴᴇ ʙᴜᴛᴛᴏɴs.
 
 <b>NOTE:</b>
-1. Only admins can add a connection.
-2. Send <code>/connect</code> for connecting me to ur PM
+1. Tᴇʟᴇɢʀᴀᴍ ᴡɪʟʟ ɴᴏᴛ ᴀʟʟᴏᴡs ʏᴏᴜ ᴛᴏ sᴇɴᴅ ʙᴜᴛᴛᴏɴs ᴡɪᴛʜᴏᴜᴛ ᴀɴʏ ᴄᴏɴᴛᴇɴᴛ, sᴏ ᴄᴏɴᴛᴇɴᴛ ɪs ᴍᴀɴᴅᴀᴛᴏʀʏ.
+2. Mᴇᴘʜɪsᴛᴏ sᴜᴘᴘᴏʀᴛs ʙᴜᴛᴛᴏɴs ᴡɪᴛʜ ᴀɴʏ ᴛᴇʟᴇɢʀᴀᴍ ᴍᴇᴅɪᴀ ᴛʏᴘᴇ.
+3. Bᴜᴛᴛᴏɴs sʜᴏᴜʟᴅ ʙᴇ ᴘʀᴏᴘᴇʀʟʏ ᴘᴀʀsᴇᴅ ᴀs ᴍᴀʀᴋᴅᴏᴡɴ ғᴏʀᴍᴀᴛ
 
-<b>Commands and Usage:</b>
-• /connect  - <code>connect a particular chat to your PM</code>
-• /disconnect  - <code>disconnect from a chat</code>
-• /connections - <code>list all your connections</code>"""
-    EXTRAMOD_TXT = """Help: <b>Extra Modules</b>
+<b>URL Bᴜᴛᴛᴏɴs:</b>
+<code>[Bᴜᴛᴛᴏɴ Tᴇxᴛ](buttonurl:https//t.me/Mephistov3_bot)</code>
 
-<b>NOTE:</b>
-these are the extra features of Mephisto
-
-<b>Commands and Usage:</b>
-• /id - <code>get id of a specifed user.</code>
-• /info  - <code>get information about a user.</code>
-• /imdb  - <code>get the film information from IMDb source.</code>
-• /search  - <code>get the film information from various sources.</code>"""
-    ADMIN_TXT = """Help: <b>Admin mods</b>
+<b>Aʟᴇʀᴛ ʙᴜᴛᴛᴏɴs:</b>
+<code>[Bᴜᴛᴛᴏɴ Tᴇxᴛ](buttonalert:This is an alert message)</code>"""
+    AUTOFILTER_TXT = """Hᴇʟᴘ: <b>Aᴜᴛᴏ Fɪʟᴛᴇʀ</b>
 
 <b>NOTE:</b>
-This module only works for my admins
+1. Mᴀᴋᴇ ᴍᴇ ᴛʜᴇ ᴀᴅᴍɪɴ ᴏғ ʏᴏᴜʀ ᴄʜᴀɴɴᴇʟ ɪғ ɪᴛ's ᴘʀɪᴠᴀᴛᴇ.
+2. ᴍᴀᴋᴇ sᴜʀᴇ ᴛʜᴀᴛ ʏᴏᴜʀ ᴄʜᴀɴɴᴇʟ ᴅᴏᴇs ɴᴏᴛ ᴄᴏɴᴛᴀɪɴs ᴄᴀᴍ ʀɪᴘ, ᴘᴏʀɴ ᴀɴᴅ ғᴀᴋᴇ ғɪʟᴇs.
+3. Fᴏʀᴡᴀʀᴅ ᴛʜᴇ ʟᴀsᴛ ᴍᴇssᴀɢᴇ ᴛᴏ ᴍᴇ ᴡɪᴛʜ ᴏ̨ᴜᴏᴛᴇs.
+ I'ʟʟ ᴀᴅᴅ ᴀʟʟ ᴛʜᴇ ғɪʟᴇs ɪɴ ᴛʜᴀᴛ ᴄʜᴀɴɴᴇʟ ᴛᴏ ᴍʏ ᴅʙ.."""
+    CONNECTION_TXT = """Hᴇʟᴘ: <b>Cᴏɴɴᴇᴄᴛɪᴏɴs</b>
 
-<b>Commands and Usage:</b>
-• /logs - <code>to get the rescent errors</code>
-• /stats - <code>to get status of files in db.</code>
-• /users - <code>to get list of my users and ids.</code>
-• /chats - <code>to get list of the my chats and ids </code>
-• /leave  - <code>to leave from a chat.</code>
-• /disable  -  <code>do disable a chat.</code>
-• /ban  - <code>to ban a user.</code>
-• /unban  - <code>to unban a user.</code>
-• /channel - <code>to get list of total connected channels</code>
-• /broadcast - <code>to broadcast a message to all users</code>"""
+- Usᴇᴅ ᴛᴏ ᴄᴏɴɴᴇᴄᴛ ʙᴏᴛ ᴛᴏ PM ғᴏʀ ᴍᴀɴᴀɢɪɴɢ ғɪʟᴛᴇʀs 
+- ɪᴛ ʜᴇʟᴘs ᴛᴏ ᴀᴠᴏɪᴅ sᴘᴀᴍᴍɪɴɢ ɪɴ ɢʀᴏᴜᴘs.
+
+<b>NOTE:</b>
+1. Oɴʟʏ ᴀᴅᴍɪɴs ᴄᴀɴ ᴀᴅᴅ ᴀ ᴄᴏɴɴᴇᴄᴛɪᴏɴ.
+2. Sᴇɴᴅ <code>/connect</code> ғᴏʀ ᴄᴏɴɴᴇᴄᴛɪɴɢ ᴍᴇ ᴛᴏ ᴜʀ PM
+
+<b>Cᴏᴍᴍᴀɴᴅs ᴀɴᴅ Usᴀɢᴇ:</b>
+• /connect  - <code>ᴄᴏɴɴᴇᴄᴛ ᴀ ᴘᴀʀᴛɪᴄᴜʟᴀʀ ᴄʜᴀᴛ ᴛᴏ ʏᴏᴜʀ PM</code>
+• /disconnect  - <code>ᴅɪsᴄᴏɴɴᴇᴄᴛ ғʀᴏᴍ ᴀ ᴄʜᴀᴛ</code>
+• /connections - <code>ʟɪsᴛ ᴀʟʟ ʏᴏᴜʀ ᴄᴏɴɴᴇᴄᴛɪᴏɴs</code>"""
+    EXTRAMOD_TXT = """Hᴇʟᴘ: <b>Exᴛʀᴀ Mᴏᴅᴜʟᴇs</b>
+
+<b>NOTE:</b>
+ᴛʜᴇsᴇ ᴀʀᴇ ᴛʜᴇ ᴇxᴛʀᴀ ғᴇᴀᴛᴜʀᴇs ᴏғ Mᴇᴘʜɪsᴛᴏ
+
+<b>Cᴏᴍᴍᴀɴᴅs ᴀɴᴅ Usᴀɢᴇ:</b>
+• /id - <code>ɢᴇᴛ ɪᴅ ᴏғ ᴀ sᴘᴇᴄɪғᴇᴅ ᴜsᴇʀ.</code>
+• /info  - <code>ɢᴇᴛ ɪɴғᴏʀᴍᴀᴛɪᴏɴ ᴀʙᴏᴜᴛ ᴀ ᴜsᴇʀ.</code>
+• /imdb  - <code>ɢᴇᴛ ᴛʜᴇ ғɪʟᴍ ɪɴғᴏʀᴍᴀᴛɪᴏɴ ғʀᴏᴍ IMDʙ sᴏᴜʀᴄᴇ.</code>
+• /search  - <code>ɢᴇᴛ ᴛʜᴇ ғɪʟᴍ ɪɴғᴏʀᴍᴀᴛɪᴏɴ ғʀᴏᴍ ᴠᴀʀɪᴏᴜs sᴏᴜʀᴄᴇs.</code>"""
+    ADMIN_TXT = """Hᴇʟᴘ: <b>Aᴅᴍɪɴ ᴍᴏᴅs</b>
+
+<b>NOTE:</b>
+Tʜɪs ᴍᴏᴅᴜʟᴇ ᴏɴʟʏ ᴡᴏʀᴋs ғᴏʀ ᴍʏ ᴀᴅᴍɪɴs
+
+<b>Cᴏᴍᴍᴀɴᴅs ᴀɴᴅ Usᴀɢᴇ:</b>
+• /logs - <code>ᴛᴏ ɢᴇᴛ ᴛʜᴇ ʀᴇsᴄᴇɴᴛ ᴇʀʀᴏʀs</code>
+• /stats - <code>ᴛᴏ ɢᴇᴛ sᴛᴀᴛᴜs ᴏғ ғɪʟᴇs ɪɴ ᴅʙ.</code>
+• /users - <code>ᴛᴏ ɢᴇᴛ ʟɪsᴛ ᴏғ ᴍʏ ᴜsᴇʀs ᴀɴᴅ ɪᴅs.</code>
+• /chats - <code>ᴛᴏ ɢᴇᴛ ʟɪsᴛ ᴏғ ᴛʜᴇ ᴍʏ ᴄʜᴀᴛs ᴀɴᴅ ɪᴅs </code>
+• /leave  - <code>ᴛᴏ ʟᴇᴀᴠᴇ ғʀᴏᴍ ᴀ ᴄʜᴀᴛ.</code>
+• /disable  -  <code>ᴅᴏ ᴅɪsᴀʙʟᴇ ᴀ ᴄʜᴀᴛ.</code>
+• /ban  - <code>ᴛᴏ ʙᴀɴ ᴀ ᴜsᴇʀ.</code>
+• /unban  - <code>ᴛᴏ ᴜɴʙᴀɴ ᴀ ᴜsᴇʀ.</code>
+• /channel - <code>ᴛᴏ ɢᴇᴛ ʟɪsᴛ ᴏғ ᴛᴏᴛᴀʟ ᴄᴏɴɴᴇᴄᴛᴇᴅ ᴄʜᴀɴɴᴇʟs</code>
+• /broadcast - <code>ᴛᴏ ʙʀᴏᴀᴅᴄᴀsᴛ ᴀ ᴍᴇssᴀɢᴇ ᴛᴏ ᴀʟʟ ᴜsᴇʀs</code>"""
     STATUS_TXT = """★ 𝚃𝙾𝚃𝙰𝙻 𝙵𝙸𝙻𝙴𝚂: <code>{}</code>
 ★ 𝚃𝙾𝚃𝙰𝙻 𝚄𝚂𝙴𝚁𝚂: <code>{}</code>
 ★ 𝚃𝙾𝚃𝙰𝙻 𝙲𝙷𝙰𝚃𝚂: <code>{}</code>
 ★ 𝚄𝚂𝙴𝙳 𝚂𝚃𝙾𝚁𝙰𝙶𝙴: <code>{}</code> 𝙼𝚒𝙱
 ★ 𝙵𝚁𝙴𝙴 𝚂𝚃𝙾𝚁𝙰𝙶𝙴: <code>{}</code> 𝙼𝚒𝙱"""
-    LOG_TEXT_G = """#NewGroup
-Group = {}(<code>{}</code>)
-Total Members = <code>{}</code>
-Added By - {}
+    LOG_TEXT_G = """#NᴇᴡGʀᴏᴜᴘ
+Gʀᴏᴜᴘ = {}(<code>{}</code>)
+Tᴏᴛᴀʟ Mᴇᴍʙᴇʀs = <code>{}</code>
+Aᴅᴅᴇᴅ Bʏ - {}
 """
-    LOG_TEXT_P = """#NewUser
+    LOG_TEXT_P = """#NᴇᴡUsᴇʀ
 ID - <code>{}</code>
-Name - {}
+Nᴀᴍᴇ - {}
 """
